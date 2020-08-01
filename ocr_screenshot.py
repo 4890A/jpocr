0db7f0116cca061cc5bb436cf5fdd717b6d4e787
4890A <alexclass2015@gmail.com>
2020-07-31 22:50:05 -0400

    recognized characters are added to the clipboard

--- ocr_screenshot.py
+++ ocr_screenshot.py
@@ -7,8 +7,10 @@ import textwrap
 from google.cloud import vision
 from google.cloud.vision import types
 from PIL import Image, ImageDraw
+import pyperclip
 
 import pprint
+
 pp = pprint.PrettyPrinter(depth=6)
 
 
@@ -16,6 +18,7 @@ import wx
 from PIL import Image, ImageGrab
 
 import pykakasi
+
 kks = pykakasi.kakasi()
 
 global selectionOffset, selectionSize, c1x, c2x, c1y, c2y
@@ -27,6 +30,8 @@ c2y = 0
 selectionOffset = ""
 selectionSize = ""
 
+clipboard_buffer = ""
+
 class SelectableFrame(wx.Frame):
 
     c1 = None
@@ -36,8 +41,8 @@ class SelectableFrame(wx.Frame):
         wx.Frame.__init__(self, parent, id, title, size=wx.DisplaySize())
         self.menubar = wx.MenuBar(wx.MB_DOCKABLE)
         self.filem = wx.Menu()
-        self.filem.Append(wx.ID_EXIT, '&Transparency')
-        self.menubar.Append(self.filem, '&File')
+        self.filem.Append(wx.ID_EXIT, "&Transparency")
+        self.menubar.Append(self.filem, "&File")
         self.SetMenuBar(self.menubar)
         self.Bind(wx.EVT_MOTION, self.OnMouseMove)
         self.Bind(wx.EVT_LEFT_DOWN, self.OnMouseDown)
@@ -73,46 +78,44 @@ class SelectableFrame(wx.Frame):
     def OnPaint(self, event):
         global selectionOffset, selectionSize
         global c1x, c2x, c1y, c2y
-        if self.c1 is None or self.c2 is None: return
+        if self.c1 is None or self.c2 is None:
+            return
 
         dc = wx.PaintDC(self)
-        dc.SetPen(wx.Pen('red', 3))
+        dc.SetPen(wx.Pen("red", 3))
         dc.SetBrush(wx.Brush(wx.Colour(0, 0, 0), wx.TRANSPARENT))
 
-        dc.DrawRectangle(self.c1.x, self.c1.y, self.c2.x - self.c1.x, self.c2.y - self.c1.y)
+        dc.DrawRectangle(
+            self.c1.x, self.c1.y, self.c2.x - self.c1.x, self.c2.y - self.c1.y
+        )
 
         c1x = self.c1.x
         c1y = self.c1.y
         c2x = self.c2.x
         c2y = self.c2.y
         selectionOffset = str(self.c1.x) + "x" + str(self.c1.y)
-        selectionSize = str(abs(self.c2.x - self.c1.x)) + "x" + str(abs(self.c2.y - self.c1.y))
+        selectionSize = (
+            str(abs(self.c2.x - self.c1.x)) + "x" + str(abs(self.c2.y - self.c1.y))
+        )
 
     def PrintPosition(self, pos):
         return str(pos.x) + "x" + str(pos.y)
 
 
 class MyApp(wx.App):
-
     def OnInit(self):
         frame = SelectableFrame()
 
         return True
 
 
-app = MyApp(redirect=False)
-app.MainLoop()
-im2 = ImageGrab.grab(bbox=(c1x+10, c1y+35, c2x+15, c2y+50))
-im2.save('temp.png')
-
-
-def get_document_bounds(image_file):
+def recognize_image(image_file, clipboard_buffer):
     """Returns document bounds given an image."""
     client = vision.ImageAnnotatorClient()
 
     bounds = []
 
-    with io.open(image_file, 'rb') as image_file:
+    with io.open(image_file, "rb") as image_file:
         content = image_file.read()
 
     image = types.Image(content=content)
@@ -131,7 +134,6 @@ def get_document_bounds(image_file):
         for block in page.blocks:
             results = []
             results.append([])
-            print('\n')
             for paragraph in block.paragraphs:
                 for word in paragraph.words:
                     for symbol in word.symbols:
@@ -147,44 +149,60 @@ def get_document_bounds(image_file):
             s.Pen = wx.Pen("#FF0000")
             s.SetTextForeground((255, 0, 0))
             s.SetTextBackground((0, 0, 0))
-            s.Brush = wx.Brush(wx.Colour(255,255,255))
+            s.Brush = wx.Brush(wx.Colour(255, 255, 255))
 
-            s.SetFont(wx.Font(10, wx.FONTFAMILY_DECORATIVE, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD))
+            s.SetFont(
+                wx.Font(
+                    10,
+                    wx.FONTFAMILY_DECORATIVE,
+                    wx.FONTSTYLE_NORMAL,
+                    wx.FONTWEIGHT_BOLD,
+                )
+            )
 
-            # results[-1] = ''.join(results[-1])
-            ocr_results = ''.join(results[-1])
-            print(ocr_results)
+            ocr_results = "".join(results[-1])
+            clipboard_buffer = clipboard_buffer + ocr_results
+            clipboard_buffer = clipboard_buffer + "\n"
             items = kks.convert(ocr_results)
 
             for item in items:
-                print(item['orig'], end = '')
-            print('\n')
-
-            for item in items:
-                print("{}[{}] ".format(item['orig'], item['hepburn'].capitalize()), end='')
-            print('\n')
+                print(
+                    "{}[{}] ".format(item["orig"], item["hepburn"].capitalize()), end=""
+                )
+            print("\n")
             hepburn_block = ""
             for item in items:
-                print(item['hepburn'], end =' ')
-                hepburn_block = hepburn_block + " " + item['hepburn']
-            print('\n')
-            hepburn_block = '\n'.join(textwrap.wrap(hepburn_block, 25))
-            nl_separated_block = hepburn_block.split('\n')
-            max_x_bound = max([s.GetTextExtent(line)[0] for line in nl_separated_block]) + 3
-            max_y_bound = s.GetTextExtent(hepburn_block)[1] * len(nl_separated_block) + 3
+                hepburn_block = hepburn_block + " " + item["hepburn"]
 
+            hepburn_block = "\n".join(textwrap.wrap(hepburn_block, 25))
+            nl_separated_block = hepburn_block.split("\n")
+
+            max_x_bound = (
+                max([s.GetTextExtent(line)[0] for line in nl_separated_block]) + 3
+            )
+            max_y_bound = (
+                s.GetTextExtent(hepburn_block)[1] * len(nl_separated_block) + 3
+            )
             w, h, = s.GetTextExtent(hepburn_block)
 
-            s.DrawRectangle(ss_x1 + start_x - 3, ss_y1 + start_y - 3, max_x_bound, max_y_bound)
-            s.DrawText(hepburn_block, ss_x1 + start_x, ss_y1 + start_y )
+            s.DrawRectangle(
+                ss_x1 + start_x - 3, ss_y1 + start_y - 3, max_x_bound, max_y_bound
+            )
+            s.DrawText(hepburn_block, ss_x1 + start_x, ss_y1 + start_y)
+
+    return clipboard_buffer
 
 
-
-def render_doc_text(filein, fileout):
+def render_doc_text(filein, fileout, clipboard_buffer):
     image = Image.open(filein)
-    get_document_bounds(filein)
-
-render_doc_text('./temp.png', 0)
+    return recognize_image(filein, clipboard_buffer)
 
 
+app = MyApp(redirect=False)
+app.MainLoop()
 
+img = ImageGrab.grab(bbox=(c1x + 10, c1y + 35, c2x + 15, c2y + 50))
+img.save("temp.png")
+
+clipboard_buffer = render_doc_text("./temp.png", 0, clipboard_buffer)
+pyperclip.copy(clipboard_buffer)
